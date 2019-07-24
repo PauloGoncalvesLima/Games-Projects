4a64e57dfd647a90bfb8b7abdc6046365cb177f6
root <root@Paulo-Pc.localdomain>
2019-07-24 02:08:02 -0300

    Flappy Bird Terminado.

---
+++ Flappy Bird/Assets/Scripts/Sounds/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sounds[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sounds s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.Volume;
+        }
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Sounds s = Array.Find(sounds, Sounds => Sounds.Name == name);
+        s.source.Play();
+    }
+
+    public float PlayingTime(string name)
+    {
+        Sounds s = Array.Find(sounds, Sounds => Sounds.Name == name);
+        return s.source.clip.length;
+    }
+
+
+}
---
+++ Flappy Bird/Assets/Scripts/Sounds/Sounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Sounds
+{
+    public string Name;
+   
+    public AudioClip clip;
+   
+    [Range(0f, 100f)]
+    public float Volume;
+
+    [HideInInspector]
+    public AudioSource source;
+}
