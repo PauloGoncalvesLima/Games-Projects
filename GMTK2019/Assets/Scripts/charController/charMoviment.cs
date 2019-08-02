a3abda521f291d28db89313e5a5537a7aef6db7f
root <root@Paulo-Pc.localdomain>
2019-08-02 16:57:07 -0300

    Adicionando movimento do character.

---
+++ GMTK2019/Assets/Scripts/charController/charMoviment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charMoviment : MonoBehaviour
+{
+    public static bool hasLight;
+    public float velocity;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        velocity = 1.5f;
+        hasLight = true; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(hasLight == true)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, 0);
+        }
+    }
+}
